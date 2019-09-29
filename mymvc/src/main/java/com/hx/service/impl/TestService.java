38005b5e082afabcb912f302f57b4feb4f85ecc8
huangxin <1377186691@qq.com>
2019-09-29 14:15:24 +0800

    增加自定义mvc源码学习模块

---
+++ mymvc/src/main/java/com/hx/service/impl/TestService.java
@@ -0,0 +1,18 @@
+package com.hx.service.impl;
+
+import com.hx.service.ITestService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService implements ITestService {
+
+    @Override
+    public void add(String name) {
+        System.out.println("新增了" + name);
+    }
+
+    @Override
+    public void query(String name, String age) {
+        System.out.println(name + "的年龄是" + age);
+    }
+}
